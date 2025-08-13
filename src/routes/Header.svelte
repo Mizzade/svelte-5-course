8b0fd1071e088597297de44c3e104b1f64d15d99
Mirko Lauff <bleiwolle@gmail.com>
2025-08-13 11:41:38 +0200

    feat/add-first-component (#1)

---
+++ src/routes/Header.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	let { name }: { name: string } = $props();
+</script>
+
+<h1>Hello, {name}</h1>
+<h2>{name.replaceAll('t', 'X')}</h2>
+
+<style>
+	h1 {
+		color: blue;
+	}
+</style>
